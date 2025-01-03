e6c33f1bf4fa329ba3268b31f19325ebf3561428
DarrenZal <zaldarren@gmail.com>
2025-01-03 00:53:00 -0500

    Initial commit

---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,64 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  image: string;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: '',
+    image: require('@site/static/img/gaialogo.png').default,
+    description: (
+      <>
+      </>
+    ),
+  },
+  {
+    title: '',
+    image: require('@site/static/img/gaiaicon.jpg').default,
+    description: (
+      <>
+      </>
+    ),
+  },
+  {
+    title: '',
+    image: require('@site/static/img/gotyourbackmother.png').default,
+    description: (
+      <>
+      </>
+    ),
+  },
+];
+
+function Feature({title, image, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <img className={styles.featureSvg} src={image} alt={title} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
